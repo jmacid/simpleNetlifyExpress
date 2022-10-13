316bfaf06188e433349a9ba79641233921ff9d1c
Juan Martin Nazarian <jmnazarian@gmail.com>
2022-10-13 18:12:36 -0300

    change port

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 ## To test locally
 First run `npx netlify-cli dev`, and then open `dist/index.html` and see the console.
 
+## Deploy
+Drag and drop the content of the folder in netlify.
+
 ## References
 [Netlify](https://docs.netlify.com/)
 [Repo](https://github.com/jmacid/simpleNetlifyExpress)
 [Video](https://www.youtube.com/watch?v=q1TrsvKdpcU)
+[netlify-dev](https://cli.netlify.com/netlify-dev/)
--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,11 @@
 async function start() {
-  const response = await fetch('http://localhost:8888/.netlify/functions/api/json')
+  const response = await fetch('http://localhost:8885/.netlify/functions/api/json')
   const jsonRes = await response.json()
-  console.log(jsonRes);
+
+  const p = document.getElementById("responseP")
+  p.textContent = jsonRes.message
+
+  console.log(jsonRes.message);
 }
 
 start()
